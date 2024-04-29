5153a1b83bd43f5ddd096b5cf4a79e1d33c970e1
Yann Gibaja <ygibaja@gmail.com>
2024-04-29 10:21:20 -0400

    Update PuzzleSolution.java
    Fixed typo

--- PuzzleSolution.java
+++ PuzzleSolution.java
@@ -5,7 +5,7 @@ public class PuzzleSolution {
 	
 	public void Print() {
 		for (int i = 0; i < orientedPieces.length; i++) {
-			System.out.format("F%d -> (P%d, %d°)", i, orientedPieces[i].pieceIndex+1, orientedPieces[i].rotation * 90);
+			System.out.format("F%d -> (P%d, %d)", i, orientedPieces[i].pieceIndex+1, orientedPieces[i].rotation * 90);
 			if (i <= 4) 
 				System.out.print(", ");
 			else
